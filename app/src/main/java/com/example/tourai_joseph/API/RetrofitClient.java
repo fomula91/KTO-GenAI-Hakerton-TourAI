280ce65d6afa665b12cfea25c57f39b9db58d4b3
KimHyeongJoong <fomula91@gmail.com>
2023-08-12 18:38:27 +0900

    feat: add buildConfig

---
+++ app/src/main/java/com/example/tourai_joseph/API/RetrofitClient.java
@@ -0,0 +1,33 @@
+package com.example.tourai_joseph.API;
+
+import com.example.tourai_joseph.BuildConfig;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.concurrent.TimeUnit;
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = BuildConfig.BASE_URL;
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            Gson gson = new GsonBuilder().setLenient().create();
+
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(10, TimeUnit.MINUTES)
+                    .readTimeout(10, TimeUnit.MINUTES)
+                    .writeTimeout(10, TimeUnit.MINUTES)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return retrofit;
+    }
+}
